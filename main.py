4abe9caf1adcc8fae0082f17d7e21c95da3fa5b4
AdamWudarczykProjects <53154193+AdamWudarczyk1234@users.noreply.github.com>
2020-08-25 22:13:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,33 @@ wn.title("Pong by @AdamWudarczyk")
 wn.bgcolor("grey")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("red")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350,0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("red")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(+350,0)
+
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("red")
+ball.penup()
+ball.goto(0,0)
+ball.dx = 0.2
+ball.dy = -0.2
